3e366f9f50c3691c5329d0d421b6286952c0ae87
JortvD <jortvandriel@hotmail.com>
2018-11-10 18:24:01 +0100

    Move everything into their own functions

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const Utils = require("@ijo-sm/utils");
 
 let exportsMap = new Map();
 
-module.exports = function(path) {
-	if(path.startsWith("src/")) {
-        if(exportsMap.has(path)) {
-            return exportsMap.get("src/");
-        }
+function isSourcePath(path) {
+    return path.startsWith("src/");
+}
 
-        return exportsMap.set(path, require(Utils.path.resolve("./" + path))).get(path);
+function setModuleInExportsMap(path, requiredModule) {
+    exportsMap.set(path, requiredModule);
+
+    return requiredModule;
+}
+
+function getFromExportsMap(path) {
+    if(exportsMap.has(path)) {
+        return exportsMap.get("src/");
+    }
+
+    let requiredModule = require(Utils.path.resolve("./" + path));
+
+    return setModuleInExportsMap(path, requiredModule);
+}
+
+module.exports = function(path) {
+	if(isSourcePath(path)) {
+       return getFromExportsMap(path);
     }
 
     return require(path);
